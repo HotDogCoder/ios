ac3ef28aaf5f3553d55dc28c347a4d5d8e9cbceb
InsetDeflection <jorge@deflection.ai>
2024-02-09 15:55:56 -0500

    Initial Commit

---
+++ store/storeApp.swift
@@ -0,0 +1,17 @@
+//
+//  storeApp.swift
+//  store
+//
+//  Created by Jorge Hospinal Flores on 9/02/24.
+//
+
+import SwiftUI
+
+@main
+struct storeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
